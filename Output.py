72e7c93411c3eabd82cdeacb5ca7af13045a7a5f
NTFS2020 <HTTcode2020@126.com>
2022-02-12 15:27:23 +0800

    Upload

--- Output.py
+++ Output.py
@@ -1,14 +1,18 @@
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
-import os,sys,time
 from ui_output import Ui_Output
 class Output(QMainWindow,Ui_Output):
     def __init__(self,server_path):
         super().__init__()
+        self.server_path = server_path
         self.setupUi(self)
+        with open(f"{server_path}server.log") as f:
+                log = f.read()
+        self.logs.setText(log)
+        self.readlog()
+    def readlog(self):
         while True:
-            with open(f"{server_path}server.log") as f:
+            with open(f"{self.server_path}server.log") as f:
                 log = f.read()
             self.logs.setText(log)
-            time.sleep(2)
