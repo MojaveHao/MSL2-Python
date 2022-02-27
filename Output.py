759a8f67b4628c07afc02806dda5fa5bcffd1db3
NTFS2020 <62271303+NTFS2020@users.noreply.github.com>
2022-02-27 17:57:53 +0800

    Delete Output.py

--- Output.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import *
-from PySide6.QtGui import *
-from PySide6.QtCore import *
-from ui_output import Ui_Output
-class Output(QMainWindow, Ui_Output):
-    def __init__(self,server_path):
-        super().__init__()
-        self.server_path = server_path
-        self.setupUi(self)
