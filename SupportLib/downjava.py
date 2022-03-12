fa82679a0b3e93e3e34a79d5239a7db8c2884361
2z0h0m9 <80618532+2z0h0m9@users.noreply.github.com>
2022-03-12 21:05:21 +0800

    downjava.py

---
+++ SupportLib/downjava.py
@@ -0,0 +1,6 @@
+import os
+from download import download
+
+def setUp():
+    download('http://ftp.de.debian.org/debian/pool/main/o/openjdk-17/openjdk-17-jre-headless_17~19-1_arm64.deb', 'Java17.deb')
+    os.system('sudo dpkg -i Java17.deb')
