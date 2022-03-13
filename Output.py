f5b65ea2d89175cb3808b3fd2dfc2452ef04cf69
NTFS2020 <62271303+NTFS2020@users.noreply.github.com>
2022-03-13 14:07:42 +0800

    Dev Update

--- Output.py
+++ Output.py
@@ -2,8 +2,36 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 from ui_output import Ui_Output
-class Output(QMainWindow, Ui_Output):
-    def __init__(self,server_path):
+class Output(QDialog, Ui_Output):
+    def __init__(self,server_path,stat):
         super().__init__()
-        self.server_path = server_path
         self.setupUi(self)
+        self.show()
+        if stat == True:
+            self.server_path = server_path
+            path = self.server_path
+            self.read_logs = ReadingLogs(self.server_path)
+            reading = QThread.start(self.read_logs)
+        else:
+            QMessageBox.warning(self,"警告","请先开启服务器")
+class ReadingLogs(QThread):
+    def __init__(self,path):
+        super().__init__()
+        self.path = path
+    def run(self,path):
+        jump = 0
+        while True:
+            with open(f"{path}server.log") as f:
+                if jump > 0:
+                    for i in range(jump):
+                        f.next()
+                    try:
+                        for whe in range(1, 50):
+                            new_logs = f.readline()
+                    except:
+                        jump = whe
+                else:
+                    for whe in range(1, 50):
+                        new_logs = f.readline()
+            self.new_logs = new_logs
+            self.show_logs.setText(self.new_logs)
--- create_config.py
+++ create_config.py
@@ -1,7 +1,7 @@
 def read_config():
     with open("msl_config.txt") as f:
         temp = f.read()
-    return (temp[0]["ujv"],temp[0]["jp"],temp[0]["sp"],temp[0]["dp"])
+    return (temp["ujv"],temp[0]["jp"],temp[0]["sp"],temp[0]["dp"])
 def write_config(ujv,jp,sp,dp):
     with open("msl_config.txt","w") as f:
-        f.write([{"ujv":ujv,"jp":jp,"sp":sp,"dp":dp}])
+        f.write(f"['ujv':{ujv},'jp':{jp},'sp':{sp},'dp':{dp}]")
