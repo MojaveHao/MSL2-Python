95da58ecd896160a5d2804465664f853747430e8
xianyongjiian080402 <xianyongjian080402@163.com>
2022-07-30 02:30:45 +0800

    修bug

--- download_server_support.py
+++ download_server_support.py
@@ -16,7 +16,8 @@ class Download_Manager(QDialog, Ui_Dialog):
     
     def download(self):
         type = self.cbox_type.currentText()
-        version = self.comboBox.currentText()
+        version = self.cbox_source.currentText()
         url, name = get_file_url(type, version)
+        # 上面那个还是有问题 我明天修（困
         download(url, name, download_path=self.server_path)
         print("请等待在命令行中下载文件,下载完成后将存放在MSLDownload目录下")
