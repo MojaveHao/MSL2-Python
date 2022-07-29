cc2f55e1fdeebaa8128ff585b626c962e284828a
xianyongjiian080402 <xianyongjian080402@163.com>
2022-07-29 20:46:13 +0800

    Ui更新

--- .qt_for_python/uic/output1.py
+++ .qt_for_python/uic/output1.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'output.ui'
+## Form generated from reading UI file 'output1.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.3.0
 ##
